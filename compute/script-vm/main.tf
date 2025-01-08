bb75aec7f18f9bc18c04e0a1e62a3a8ed93b91c6
richrose <askrichardrose@gmail.com>
2025-01-08 22:53:26 +0000

    Add: Example script host

---
+++ compute/script-vm/main.tf
@@ -0,0 +1,55 @@
+# Create the Free Tier compute instance
+resource "google_compute_instance" "free_tier_vm" {
+  name         = var.gce_instance_name
+  machine_type = var.gce_machine_type # Free tier eligible machine type
+  zone         = var.gce_machine_zone # Choose a zone within the selected region
+
+  boot_disk {
+    initialize_params {
+      # https://cloud.google.com/compute/docs/images/os-details
+      # image = var.gce_public_image
+      image = data.google_compute_image.debian.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Allow external access to the VM
+      # nat_ip = "EXTERNAL"
+    }
+  }
+
+  # Run Script
+  # metadata = {
+  #  startup-script = file("scripts/lab-init.sh") 
+  # }
+
+  metadata = {
+    startup-script = <<EOF
+      #!/bin/bash
+      # STARTUP-START
+      # Update package lists and install required packages
+      # Env Var
+      export USER="nix-dev"
+      export HOME="/home/$USER"
+
+      # Create a user
+      useradd $USER -m -p Password01 -s /bin/bash -c "$USER Developer Account"
+
+      # Install Nix package manager - Ensure $USER + $HOME env var are defined
+      sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+
+      # Install required application packages
+      /nix/var/nix/profiles/default/bin/nix-env -iA nixpkgs.nodejs_22 nixpkgs.firebase-tools nixpkgs.cacert
+      # nix-env -iA nixpkgs.nodejs_22 nixpkgs.firebase-tools nixpkgs.cacert
+    EOF
+  }
+
+
+}
+
+data "google_compute_image" "debian" {
+  project = "debian-cloud"
+  family  = "debian-11"
+}
---
+++ compute/script-vm/variables.tf
@@ -0,0 +1,59 @@
+# Default value passed in
+variable "gcp_project_id" {
+  type        = string
+  description = "The GCP project ID to create resources in."
+  default = "qwiklabs-gcp-00-fd4dbf174dfb"
+}
+
+# Default value passed in
+variable "gcp_region" {
+  type        = string
+  description = "Region to create resources in."
+  default = "us-centtral1"
+}
+
+## Google Compute Engine properties
+## -------------------------------------------------------
+
+# Default value passed in
+variable "gce_instance_name" {
+  type        = string
+  description = "Google Cloud Engine instance name."
+  default     = "dev-vm"
+}
+
+# Default value passed in
+variable "gce_machine_type" {
+  type        = string
+  description = "Google Cloud Engine machine type."
+  default     = "e2-medium"
+  # default     = "e2-small"
+}
+
+# Default value passed in
+variable "gce_machine_zone" {
+  type        = string
+  description = "Google Cloud Engine machine type."
+  default = "us-central1-a"
+}
+
+# Default value passed in
+# Ref: https://cloud.google.com/compute/docs/images/os-details
+variable "gce_public_image" {
+  type        = string
+  description = "Google Cloud Engine compute image."
+  default = "debian-cloud/debian-11" 
+  # default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+variable "gce_project" {
+  type        = string
+  description = "GCE Project."
+  default = "debian-cloud"
+}
+
+variable "gce_image" {
+  type        = string
+  description = "Google Cloud Engine compute image."
+  default = "debian-11"
+}
